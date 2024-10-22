8867818aa715a2b5dcf036eee12c48d3d95251ab
sebastien duvivier <sduvivier59112@gmail.com>
2024-10-22 18:52:44 +0200

    update

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,1 +1,26 @@
 
+	// Get modal element
+	const modal = document.getElementById("myModal");
+
+	// Get button that opens the modal
+	const openModalBtn = document.getElementById("openModalBtn");
+
+	// Get the <span> element that closes the modal
+	const closeBtn = document.getElementsByClassName("close")[0];
+
+	// Open modal when button is clicked
+	openModalBtn.onclick = function() {
+		modal.style.display = "block";
+	}
+
+	// Close the modal when <span> (x) is clicked
+	closeBtn.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// Close the modal when clicking outside of the modal content
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
